cc26e40ce5d17fff066439498e91208995e5d2fd
Keshawn <Keshawn287@gmail.com>
2025-11-16 23:41:56 -0500

    Game-Card

--- nihon-learning-games/src/components/GameCard.jsx
+++ nihon-learning-games/src/components/GameCard.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
 
-export default function GameCard({ to, title, description }) {
+export default function GameCard({ to, title, level, description }) {
   return (
     <Link to={to} className="game-card">
-      <h2>{title}</h2>
-      <p>{description}</p>
+      <div className="game-card-header">
+        <h2>{title}</h2>
+        <span className="game-card-level">{level}</span>
+      </div>
+      <p className="game-card-description">{description}</p>
     </Link>
   );
 }
